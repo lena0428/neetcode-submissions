1f764ce1bb734d72643fcbbd4ebf75fc4ab76af5
lena0428 <139748662+lena0428@users.noreply.github.com>
2026-05-10 16:19:46 -0700

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+         if (edges.length > n - 1) {
+            return false;
+        }
+        List<List<Integer>> graph = new ArrayList<>();
+        // build graph
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] edge: edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        HashSet<Integer> visited = new HashSet<>();
+         if (!dfs(0, -1, graph, visited)) {
+            return false;
+        }
+
+        return visited.size() == n;
+    }
+
+    // cycle
+    public boolean dfs(int cur, int parent, List<List<Integer>> graph, HashSet<Integer> visited) {
+        if (visited.contains(cur)) {
+            return false;
+        }
+        visited.add(cur);
+        for(int neighbor: graph.get(cur)) {
+            // cycle
+            if (neighbor == parent) {
+                return false;
+            }
+            dfs(neighbor, parent, graph, visited);
+        }
+        return true;
+    }
+}
