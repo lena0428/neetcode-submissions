ab03642f2236d1925cf9e66d903bf9845d11350a
lena0428 <139748662+lena0428@users.noreply.github.com>
2026-05-10 22:49:36 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,25 @@
+class TimeMap {
+    HashMap<String, TreeMap<Integer, String>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) {
+            map.put(key, new TreeMap<>());
+        }
+        map.get(key).put(timestamp, value);
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key)) {
+            return "";
+        }
+        Integer i = map.get(key).floorKey(timestamp);
+        if (i == null) {
+            return "";
+        }
+        return map.get(key).get(i);
+    }
+}
