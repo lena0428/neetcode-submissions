8342f3acb373a6c8ce58b0f28530791c4d0d4e2e
lena0428 <139748662+lena0428@users.noreply.github.com>
2026-05-25 21:53:23 -0700

    Add: merge-intervals - submission-9

---
+++ Data Structures & Algorithms/merge-intervals/submission-9.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int left = intervals[0][0];
+        int right = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (right >= intervals[i][0]) {
+                right = Math.max(intervals[i][1], right);
+            } else {
+                res.add(new int[]{left, right});
+                left = intervals[i][0];
+                right = intervals[i][1];
+            }
+        }
+        res.add(new int[]{left, right});
+        return res.toArray(new int[res.size()][]);
+    }
+}
